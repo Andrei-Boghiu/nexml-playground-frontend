6c79b38bcd4f4fb5a647097c9aacbe457bdca0b4
Andrew <vandrei1408@yahoo.com>
2025-10-12 02:33:09 +0300

    feat: #4 add job listing pages for CRUD

--- src/pages/JobListing.tsx
+++ src/pages/JobListing.tsx
@@ -1,3 +1,117 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { Spinner } from "@/components/ui/spinner";
+
+import { deleteJobListing, getJobListing, updateJobListing } from "@/services/job-listing.service";
+
+import LastUpdatedAtBadge from "@/components/common/LastUpdatedAtBadge";
+import ItemContentEditor from "@/components/views/item-content-editor";
+import ItemPageHeader from "@/components/views/item-page-header";
+import { Badge } from "@/components/ui/badge";
+
+import type { UpdateJobListingFormData } from "@/schemas/job-listing.schema";
+import type { ApiError } from "@/types/types";
+
 export default function JobListing() {
-  return <div>JobListing</div>;
+  const [description, setDescription] = useState<string>("");
+  const [position, setPosition] = useState<string>("");
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const { id } = useParams<{ id: string }>() as { id: string };
+
+  const queryKey = ["job-listings", id];
+
+  const {
+    data: jobListing,
+    isLoading,
+    dataUpdatedAt,
+    isFetching,
+    refetch,
+    isError,
+    error,
+  } = useQuery({
+    queryKey,
+    queryFn: () => getJobListing(id!),
+    enabled: Boolean(id),
+    staleTime: Infinity,
+  });
+
+  useEffect(() => {
+    if (jobListing) {
+      setPosition(jobListing.position);
+      setDescription(jobListing.description);
+    }
+  }, [jobListing]);
+
+  const { mutate: saveJobListing, isPending: isSaving } = useMutation({
+    mutationFn: (updated: UpdateJobListingFormData) => updateJobListing(id!, updated),
+    onSuccess: (updated) => {
+      toast.success("Job listing updated successfully!");
+      queryClient.setQueryData(queryKey, updated);
+    },
+    onError: (err: ApiError) => {
+      const description = err?.response?.data?.error || err.message;
+      toast.error("Error updating job listing", { description });
+    },
+  });
+
+  const { mutate: deleteJobListingMutate, isPending: isDeleting } = useMutation({
+    mutationFn: () => deleteJobListing(id!),
+    onSuccess: () => {
+      toast.success("Job listing deleted successfully!");
+      queryClient.invalidateQueries({ queryKey: ["job-listings"] });
+      navigate("/job-listings");
+    },
+    onError: (err: ApiError) => {
+      const description = err?.response?.data?.error || err.message;
+      toast.error("Error deleting job listing", { description });
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-16">
+        <Spinner scale={148} />
+      </div>
+    );
+  }
+
+  if (isError) {
+    const errMsg = (error as ApiError).response?.data?.error || error?.message;
+
+    return (
+      <div className="text-center text-red-500 py-16">
+        <p>Error: {errMsg}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="space-y-6 m-3.5">
+      <ItemPageHeader
+        name={position}
+        isSaving={isSaving}
+        isDeleting={isDeleting}
+        onDelete={deleteJobListingMutate}
+        onSave={async (newPosition) => saveJobListing({ position: newPosition })}
+        refetch={refetch}
+        isFetching={isFetching}
+      />
+
+      <div className="flex flex-col gap-4">
+        <Badge variant="secondary">Created at: {new Date(jobListing?.createdAt).toLocaleString()}</Badge>
+        <Badge variant="secondary">Updated at: {new Date(jobListing?.updatedAt).toLocaleString()}</Badge>
+        <LastUpdatedAtBadge dataUpdatedAt={dataUpdatedAt} />
+      </div>
+
+      <ItemContentEditor
+        content={description}
+        isSaving={isSaving}
+        onSave={async (newDescription) => saveJobListing({ description: newDescription })}
+      />
+    </div>
+  );
 }
--- src/pages/JobListingList.tsx
+++ src/pages/JobListingList.tsx
@@ -1,3 +1,113 @@
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+import InfoActionBar from "@/components/common/InfoActionBar";
+import ContentCardGrid from "@/components/common/ContentCardGrid";
+import PaginationBottomBar from "@/components/common/PaginationBottomBar";
+import CreateItemSideSheet, { type NewItemField } from "@/components/common/CreateItemSideSheet";
+
+import type { PaginatedRequest } from "@/types/types";
+import { createJobListingSchema, type CreateJobListingFormData } from "@/schemas/job-listing.schema";
+import { createJobListing, getJobListings } from "@/services/job-listing.service";
+
+const configFields: Record<keyof CreateJobListingFormData, NewItemField> = {
+  position: {
+    type: "input",
+    placeholder: "Senior Sails Representative",
+  },
+  description: {
+    type: "textarea",
+    placeholder: "Professional with +10 years of experience in world wide...",
+  },
+  department: {
+    type: "input",
+    placeholder: "Sales",
+  },
+};
+
 export default function JobListingList() {
-  return <div>JobListings</div>;
+  const [isCreatingItem, setIsCreatingItem] = useState<boolean>(false);
+  const [search, setSearch] = useState<string>("");
+  const [debouncedSearch, setDebouncedSearch] = useState<string>("");
+
+  const [page, setPage] = useState<number>(1);
+  const [limit] = useState<number>(12);
+
+  const [isAddSheetOpen, setIsAddSheetOpen] = useState<boolean>(false);
+
+  const {
+    data: jobListingList,
+    refetch,
+    dataUpdatedAt,
+    error,
+    isError,
+    isRefetching,
+    isLoading,
+  } = useQuery({
+    queryKey: ["job-listings", { search: debouncedSearch, page, limit }],
+    queryFn: ({ queryKey }) => {
+      const [, params] = queryKey;
+      return getJobListings(params as PaginatedRequest);
+    },
+  });
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedSearch(search.trim());
+      setPage(1);
+    }, 500); // ⏱ debounce delay (ms)
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [search]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Error", {
+        description: error.message,
+      });
+    }
+  }, [error, isError]);
+
+  async function handlePageChange(newPage: number) {
+    if (newPage === page) return;
+
+    setPage(newPage);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  return (
+    <>
+      <InfoActionBar
+        search={search}
+        setSearch={setSearch}
+        isCreatingItem={isCreatingItem}
+        setIsAddSheetOpen={setIsAddSheetOpen}
+        query={{ dataUpdatedAt, refetch, isRefetching, isLoading }}
+      />
+
+      <ContentCardGrid data={jobListingList?.data} isLoading={isLoading} redirectEntity="job-listings" />
+
+      <PaginationBottomBar
+        totalPages={jobListingList?.meta.totalPages || 0}
+        page={page}
+        onPageChange={handlePageChange}
+      />
+
+      <CreateItemSideSheet
+        queryKeyToInvalidate="job-listings"
+        title="Create New Job Listing"
+        description="Create a new job listing item by filling in the form below. Click save when you're done."
+        onSubmit={createJobListing}
+        isLoading={isCreatingItem}
+        setIsLoading={setIsCreatingItem}
+        isOpen={isAddSheetOpen}
+        setIsOpen={setIsAddSheetOpen}
+        fields={configFields}
+        schema={createJobListingSchema}
+      />
+    </>
+  );
 }
